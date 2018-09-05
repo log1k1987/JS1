686e08df0e6a38e6f6d9119db34039889ae8d49c
log1k <log1k@mail.ru>
2018-09-05 11:35:00 +0300

    geo

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-let myMap;
-let myPlacemark;
+let myMap, myPlacemark;
 
 // Дождёмся загрузки API и готовности DOM.
 ymaps.ready(init);
@@ -7,26 +6,24 @@ ymaps.ready(init);
 function init() {
     myMap = new ymaps.Map('map', {
         center: [55.76, 37.64],
-        zoom: 10
-    }, {
-        searchControlProvider: 'yandex#search'
+        zoom: 10,
+        controls: ['zoomControl', 'searchControl'],
     });
 
     // Слушаем клик на карте.
-    myMap.events.add('click', function (e) {
-        var coords = e.get('coords');
-        var position = e.get('position');
+    myMap.events.add('click', e => {
+        let coords = e.get('coords'),
+            position = e.get('position');
 
         baseInformation(coords, position);
         clusterer.balloon.close();
-
     });
 
-    var customItemContentLayout = ymaps.templateLayoutFactory.createClass(
+    let customItemContentLayout = ymaps.templateLayoutFactory.createClass(
         // Флаг "raw" означает, что данные вставляют "как есть" без экранирования html.
-        '<h2 class=ballon_header>{{ properties.balloonContentHeader|raw }}</h2>' +
-        '<div class=ballon_body>{{ properties.balloonContentBody|raw }}</div>' +
-        '<div class=ballon_footer>{{ properties.balloonContentFooter|raw }}</div>'
+        '<h2 class=ballon__header>{{ properties.balloonContentHeader|raw }}</h2>' +
+            '<div class=ballon__body>{{ properties.balloonContentBody|raw }}</div>' +
+            '<div class=ballon__footer>{{ properties.balloonContentFooter|raw }}</div>'
     );
 
     clusterer = new ymaps.Clusterer({
@@ -34,56 +31,46 @@ function init() {
         clusterDisableClickZoom: true,
         openBalloonOnClick: true,
         clusterBalloonContentLayout: 'cluster#balloonCarousel',
-        clusterBalloonItemContentLayout: customItemContentLayout
+        clusterBalloonItemContentLayout: customItemContentLayout,
     });
-
-
 }
 
 let information = {};
-
 
 //наполнение массива информацией
 function baseInformation(coords, position) {
     geocode(coords);
     information[`${coords[0]}-${coords[1]}`] = {};
     information[`${coords[0]}-${coords[1]}`].position = position;
-
 }
-
 
 //получение адреса
 function geocode(coords) {
     ymaps.geocode(coords).then(res => {
-        var firstGeoObject = res.geoObjects.get(0);
+        let firstGeoObject = res.geoObjects.get(0);
         information[`${coords[0]}-${coords[1]}`].address = firstGeoObject.getAddressLine();
         creatBallon(coords);
     });
 }
 
-
 //создание основного блока
 function creatBallon(coords) {
-
-    let wrapper = document.querySelector('.review-elem');
-    let reviewBlockWidth = 320;
-    let reviewBlockHeigth = 539;
-    let item = information[`${coords[0]}-${coords[1]}`];
-
-    let windowHeight = window.innerHeight
-    let windowWidth = window.innerWidth
-
-    let positionTop = item.position[1] + reviewBlockHeigth > windowHeight ? windowHeight - reviewBlockHeigth : item.position[1]
-    let positionLeft = item.position[0] + reviewBlockWidth > windowWidth ? windowWidth - reviewBlockWidth : item.position[0]
-
-
+    let wrapper = document.querySelector('.review-elem'),
+        reviewBlockWidth = 320,
+        reviewBlockHeigth = 539,
+        item = information[`${coords[0]}-${coords[1]}`],
+        windowHeight = window.innerHeight,
+        windowWidth = window.innerWidth,
+        positionTop =
+            item.position[1] + reviewBlockHeigth > windowHeight ? windowHeight - reviewBlockHeigth : item.position[1],
+        positionLeft =
+            item.position[0] + reviewBlockWidth > windowWidth ? windowWidth - reviewBlockWidth : item.position[0];
 
     wrapper.style.top = positionTop + 'px';
     wrapper.style.left = positionLeft + 'px';
-    wrapper.style.zIndex = '1';
+    wrapper.style.zIndex = '100';
 
-    wrapper.innerHTML =
-        `
+    wrapper.innerHTML = `
     <div class="review">
 
         <div class="review-header">
@@ -102,9 +89,9 @@ function creatBallon(coords) {
             <div class="review-review">
                 <p>Ваш отзыв</p>
                 <div class="review-form">
-                    <input class="review_name" type="text" placeholder="Ваше имя">
-                    <input class="review_place" type="text" placeholder="Укажите место">
-                    <textarea class="review_comment" placeholder="Поделитесь впечатлением"></textarea>
+                    <input class="review__name" type="text" placeholder="Ваше имя">
+                    <input class="review__place" type="text" placeholder="Укажите место">
+                    <textarea class="review__comment" placeholder="Поделитесь впечатлением"></textarea>
                 </div>
             </div>
         
@@ -120,21 +107,23 @@ function creatBallon(coords) {
     updateReview(coords);
 }
 
-
 //обработка блока с отзывами
 function updateReview(coords) {
-    let reviewBodyElem = document.querySelector('.review-body');
-    let item = information[`${coords[0]}-${coords[1]}`];
+    let reviewBodyElem = document.querySelector('.review-body'),
+        item = information[`${coords[0]}-${coords[1]}`];
 
     if (item.reviews) {
-
         reviewBodyElem.innerHTML = '';
 
         for (let review of item.reviews) {
             let elem = document.createElement('div');
             elem.classList.add('.review-item');
-            let item = `<p><span class="review-item_name"><b>${review.name}</b></span><span class="review-item_place">${review.place}</span><span class="review-item_date">${review.date}</span></p>
-                        <p class="review-item_review">${review.comment}</p>
+            let item = `<p><span class="review-item__name"><b>${
+                review.name
+            }</b></span><span class="review-item__place">${review.place}</span><span class="review-item__date">${
+                review.date
+            }</span></p>
+                        <p class="review-item__review">${review.comment}</p>
                     `;
             elem.innerHTML = item;
             reviewBodyElem.appendChild(elem);
@@ -142,24 +131,21 @@ function updateReview(coords) {
     }
 }
 
-
 //Обработка кнопки Добавить
 function onButtonChange(coords) {
-    let reviewElem = document.querySelector('.review-elem');
-    let buttonAdd = document.querySelector('.review-footer button');
-    let buttonClose = document.querySelector('.review-header button');
-    let nameElem = document.querySelector('.review_name');
-    let placeElem = document.querySelector('.review_place');
-    let commentElem = document.querySelector('.review_comment');
-    let item = information[`${coords[0]}-${coords[1]}`];
-    let date = new Date().toLocaleString();
-    let arr = item.reviews || [];
+    let reviewElem = document.querySelector('.review-elem'),
+        buttonAdd = document.querySelector('.review-footer button'),
+        buttonClose = document.querySelector('.review-header button'),
+        nameElem = document.querySelector('.review__name'),
+        placeElem = document.querySelector('.review__place'),
+        commentElem = document.querySelector('.review__comment'),
+        item = information[`${coords[0]}-${coords[1]}`],
+        date = new Date().toLocaleString(),
+        arr = item.reviews || [];
 
-    buttonAdd.addEventListener('click', function () {
-
+    buttonAdd.addEventListener('click', () => {
         if (nameElem.value.length == 0 || placeElem.value.length == 0 || commentElem.value.length == 0) {
-
-            return false
+            return false;
         }
 
         let review = {};
@@ -180,31 +166,30 @@ function onButtonChange(coords) {
         commentElem.value = '';
     });
 
-    buttonClose.addEventListener('click', function () {
+    buttonClose.addEventListener('click', () => {
         reviewElem.innerHTML = '';
     });
 }
 
 //Создание метки
 function createPlacemark(coords) {
-    let item = information[`${coords[0]}-${coords[1]}`];
-    let dataCoords = `${coords[0]}-${coords[1]}`;
-
-    let placeMarkData = {
-        balloonContentHeader: `${item.reviews[0].name}`,
-        balloonContentBody: `<a href="#" class="clusters-link" data-coords="${dataCoords}">${item.address}</a>${item.reviews[0].comment}`,
-        balloonContentFooter: `${item.reviews[0].date}`
-    };
-
-    let options = {
-        openBalloonOnClick: false
-    };
-
-    let myPlacemark = new ymaps.Placemark(coords, placeMarkData, options);
+    let item = information[`${coords[0]}-${coords[1]}`],
+        dataCoords = `${coords[0]}-${coords[1]}`,
+        placeMarkData = {
+            balloonContentHeader: `${item.reviews[0].name}`,
+            balloonContentBody: `<a href="#" class="clusters-link" data-coords="${dataCoords}">${item.address}</a>${
+                item.reviews[0].comment
+            }`,
+            balloonContentFooter: `${item.reviews[0].date}`,
+        },
+        options = {
+            openBalloonOnClick: false,
+        },
+        myPlacemark = new ymaps.Placemark(coords, placeMarkData, options);
 
     myMap.geoObjects.add(myPlacemark);
 
-    myPlacemark.events.add('click', function () {
+    myPlacemark.events.add('click', function() {
         creatBallon(coords);
     });
 
@@ -213,17 +198,15 @@ function createPlacemark(coords) {
 
 //Добавление метки в кластер
 function createCluster(placemarks) {
-
     clusterer.add(placemarks);
     myMap.geoObjects.add(clusterer);
 }
 
 //Слушаем клик по адресу в карусели кластера
-document.addEventListener('click', (e) => {
+document.addEventListener('click', e => {
     e.preventDefault();
 
     if (e.target.className == 'clusters-link') {
-
         let coords = e.target.dataset.coords.split('-');
 
         clusterer.balloon.close();
